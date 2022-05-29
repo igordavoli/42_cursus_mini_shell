6f801f518b46448e4a45393d99c850076edeedb7
Igor Davoli <igordavoli@ioasys.com.br>
2022-05-29 01:54:45 -0300

    add builtin exit

--- src/mini_shell.h
+++ src/mini_shell.h
@@ -6,7 +6,7 @@
 /*   By: idavoli- <idavoli-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 05:16:59 by idavoli-          #+#    #+#             */
-/*   Updated: 2022/05/26 22:22:51 by idavoli-         ###   ########.fr       */
+/*   Updated: 2022/05/29 00:56:22 by idavoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,14 @@ typedef struct s_msh
 {
 	int		argc;
 	char	**argv;
-	char	**env;
+	char	**envp;
+	t_list	*envp_lst;
 	char	*line;
 	char	**splitted_line;
 
 }	t_msh;
+
+extern t_msh	g_msh;
 
 int		is_builtin(char *cmd);
 void	execute(char **cmd);
@@ -35,5 +38,7 @@ int		exec_builtin(char **cmd);
 int		echo(char **cmd);
 int		pwd(void);
 int		cd(char *path);
+void	msh_exit(void);
+t_list	*create_list(char **strs);
 
 #endif
