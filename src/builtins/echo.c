5221ced7d1c956099a455c3de85db975860e442e
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-05-29 21:53:11 +0200

    fix echo without args

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 22:13:29 by idavoli-          #+#    #+#             */
-/*   Updated: 2022/05/29 20:54:08 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/05/29 21:49:15 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@ int	echo(char **cmd)
 	int	has_n;
 	int	i;
 
-	i = 0;
-	has_n = !ft_strncmp(cmd[1], "-n", 3);
-	if (has_n)
-		i = 1;
-	while (cmd[++i])
-	{
-		ft_printf("%s", cmd[i]);
-		if (cmd[i + 1])
-			write(1, " ", 1);
-	}
-	if (!has_n)
+	if (!cmd[1])
 		ft_putchar_fd('\n', 1);
+	else
+	{
+		i = 0;
+		has_n = !ft_strncmp(cmd[1], "-n", 3);
+		if (has_n)
+			i = 1;
+		while (cmd[++i])
+		{
+			ft_printf("%s", cmd[i]);
+			if (cmd[i + 1])
+				write(1, " ", 1);
+		}
+		if (!has_n)
+			ft_putchar_fd('\n', 1);		
+	}
 	return (0);
 }
