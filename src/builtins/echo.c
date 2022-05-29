4f6ef87b1d8b23d410994880f95657961a6ac6e8
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-05-29 20:56:44 +0200

    fix echo separated strs

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: idavoli- <idavoli-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 22:13:29 by idavoli-          #+#    #+#             */
-/*   Updated: 2022/05/21 23:59:53 by idavoli-         ###   ########.fr       */
+/*   Updated: 2022/05/29 20:54:08 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 int	echo(char **cmd)
 {
 	int	has_n;
+	int	i;
 
+	i = 0;
 	has_n = !ft_strncmp(cmd[1], "-n", 3);
-	if (*cmd)
-		ft_putstr_fd(cmd[1 + has_n], 1);
+	if (has_n)
+		i = 1;
+	while (cmd[++i])
+	{
+		ft_printf("%s", cmd[i]);
+		if (cmd[i + 1])
+			write(1, " ", 1);
+	}
 	if (!has_n)
 		ft_putchar_fd('\n', 1);
 	return (0);
