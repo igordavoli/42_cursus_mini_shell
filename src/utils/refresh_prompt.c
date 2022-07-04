1b127baa3a04c985c46e35ce48a26cd065b448da
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-07-04 02:33:12 +0200

    add color in prompt

--- src/utils/refresh_prompt.c
+++ src/utils/refresh_prompt.c
@@ -29,13 +29,15 @@ char	*refresh_prompt(void)
 {
 	char	*prompt;
 	char	cwd[PATH_MAX];
+	char	*minihell;
 
 	if (g_msh.prompt)
 		free(g_msh.prompt);
-	prompt = ft_strjoin2("", getenv("USER"), 0, 0);
-	prompt = ft_strjoin2(prompt, " at miniHell in ", 0, 0);
+	prompt = ft_strjoin2("\033[1;36m", ft_getenv("USER"), 0, 0);
+	minihell = ft_strdup("\033[m at\033[3;1;31m miniHell\033[0;m in \033[1;34m");
+	prompt = ft_strjoin2(prompt, minihell, 0, 1);
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 		prompt = ft_strjoin2(prompt, get_directory(cwd), 0, 0);
-	g_msh.prompt = ft_strjoin2(prompt, "> ", 1, 0);
+	g_msh.prompt = ft_strjoin2(prompt, " \033[0;m> ", 1, 0);
 	return (g_msh.prompt);
 }
