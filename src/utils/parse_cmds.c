e274da949b5ade2508119e5fd36b5b27f1c86a89
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-09-07 01:45:24 +0200

    fix: remove invalid read

--- src/utils/parse_cmds.c
+++ src/utils/parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 00:18:22 by idavoli-          #+#    #+#             */
-/*   Updated: 2022/09/06 22:20:36 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/09/07 01:20:06 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,9 +55,20 @@ static char	**split_space(char *str)
 	return (cmd);
 }
 
-void	parse_cmds(char *parsed_line)
+static int	parse_pipe(char *parsed_line, int start_cmd, int i)
 {
 	char	*sub_cmd;
+
+	if (parsed_line[i + 1] == '\0')
+		i++;
+	sub_cmd = ft_substr(parsed_line, start_cmd, i - start_cmd);
+	ft_dlstadd_back(&g_msh.cmds_lst, ft_dlstnew(split_space(sub_cmd)));
+	free(sub_cmd);
+	return (i + 1);
+}
+
+void	parse_cmds(char *parsed_line)
+{
 	char	quote;
 	int		start_cmd;
 	int		i;
@@ -65,21 +76,16 @@ void	parse_cmds(char *parsed_line)
 	quote = 0;
 	start_cmd = 0;
 	i = -1;
-	printf("pline: %s\n", parsed_line);
 	while (parsed_line[++i])
 	{
 		if ((parsed_line[i] == '\'' || parsed_line[i] == '\"') && quote == 0)
 			quote = parsed_line[i];
 		else if (parsed_line[i] == quote)
 			quote = 0;
-		if ((parsed_line[i] == '|' && quote == 0 ) || parsed_line[i + 1] == '\0')
-		{
-			if (parsed_line[i + 1] == '\0')
-				i++;
-			sub_cmd = ft_substr(parsed_line, start_cmd, i - start_cmd);
-			ft_dlstadd_back(&g_msh.cmds_lst, ft_dlstnew(split_space(sub_cmd)));
-			free(sub_cmd);
-			start_cmd = i + 1;
-		}
+		if ((parsed_line[i] == '|' && quote == 0) \
+		|| parsed_line[i + 1] == '\0')
+			start_cmd = parse_pipe(parsed_line, start_cmd, i);
+		if (parsed_line[i] == '\0')
+			break ;
 	}
 }
