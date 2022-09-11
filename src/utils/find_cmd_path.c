1a11e49e10d11deaec1e291fa3a0f4d12e0b5fc6
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-09-12 00:05:42 +0200

    fix: invalid read path

--- src/utils/find_cmd_path.c
+++ src/utils/find_cmd_path.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/16 02:18:04 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/09/11 03:25:18 by ldatilio         ###   ########.fr       */
+/*   Created: 2022/03/16 02:18:04 by idavoli-          #+#    #+#             */
+/*   Updated: 2022/09/11 03:48:41 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ static char	**get_paths(void)
 	t_dlist	*envs;
 
 	envs = g_msh.envp_lst;
-	while (ft_strncmp(get_node_value(envs), "PATH=", 5))
+	while (envs != NULL && ft_strncmp(get_node_value(envs), "PATH=", 5))
 		envs = envs->next;
-	return (ft_split(get_node_value(envs) + 5, ':'));
+	if (envs != NULL)
+		return (ft_split(get_node_value(envs) + 5, ':'));
+	else
+		return (ft_split("", ':'));
 }
 
 static char	*join_path(char *path, char *cmd)
